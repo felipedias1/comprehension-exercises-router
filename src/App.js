8926e903fb7a17d990f8c2926b3216b0f061c918
Felipe Dias <felipeferreiradias1@gmail.com>
2021-08-17 15:17:03 -0300

    add changes exercise part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom' 
+import About from './components/About'
+import Users from './components/Users'
+import StrictAccess from './components/StrictAccess'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter >
+        <Switch>
+          <Route path="/Users/:id" render = {(props)=> <Users {...props} greetingsMessage="Good Morning"/>} />
+          <Route path="/StrictAccess" 
+            render ={ () => ( 
+              <StrictAccess user={
+                {username:'joao', 
+                password:'1234'}}/>
+            )} 
+          />
+          <Route path="/About" component ={About} />
+          <Route exact path="/" component ={Home} /> 
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Link to="/"> Home </Link>
+        <Link to="/About"> About </Link>
+        <Link to="/Users"> Users </Link>
+        <Link to="/StrictAcess"> StrictAcess </Link>
         <h2>Home</h2>
         <p> My awesome Home component </p>
       </div>
@@ -12,3 +17,5 @@ class Home extends Component {
 }
 
 export default Home;
+
+// <Route path="/" component ={Home} /> 
